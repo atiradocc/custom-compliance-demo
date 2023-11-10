5e8d67060b9596370020aca9eabdff87f84a7f38
Antonio Tirado <antonio@cloudconformity.com>
2023-11-10 15:00:26 +1100

    Final commit

--- create-compliance-standard.py
+++ create-compliance-standard.py
@@ -11,8 +11,17 @@ def readMappingFile(mappingFileName):
             data = json.load(fp=jsonFile)
             return data
     except FileNotFoundError as e:
+        print(f'''
+Could not find custom compliance standard file.
+Filename: {e.filename}
+''')
         raise e
     except json.JSONDecodeError as e:
+        print(f'''
+\n\nError while parsing JSON file.\n
+Line:\t{e.lineno}
+Column:\t{e.colno}
+Character number:\t{e.pos}\n\n''')
         raise e
     except Exception as e:
         raise e
@@ -23,25 +32,35 @@ def callApi(apiUrl, apiKey, data):
         head = { 'Content-Type': 'application/vnd.api+json', 'Authorization': f"ApiKey {apiKey}" }
         req = urllib.request.Request(apiUrl, data=jsonData, headers=head, method='POST')
 
-        print(f'''\n\nQuerying the Cloud One Conformity API to create a custom compliance standard..\n
+        print(f'''\n\n
+Querying the Cloud One Conformity API to create a custom compliance standard..\n
     Api:\t{req.full_url}
     Method:\t{req.method}\n\n''')
 
         with urllib.request.urlopen(req) as response:
-            if response.getcode() in [ 200, 201 ]:
-                responseData = response.read()
-                data = json.loads(responseData)
-                return data
-            else:
-                raise Exception(f"HTTP error: {response.getcode()}")
+            responseData = response.read()
+            data = json.loads(responseData)
+            return data
+    except urllib.error.HTTPError as e:
+        error = json.loads(e.read())
+        print(f'''
+\n\nHTTP error code:\t{e.code}\n
+{json.dumps(error, indent=4)}\n\n
+''')
+        raise e
     except urllib.error.URLError as e:
-        print(f'An error occurred: {e}')
+        print(f'\n\nCould not reach the API: {e.reason}\n\n')
         raise e
     except json.JSONDecodeError as e:
-        print(f'An error occurred: {e}')
+        print(f'''
+\n\nError while parsing API response.\n
+Line:\t{e.lineno}
+Column:\t{e.colno}
+Message:\t{e.msg}
+Response:\n{e.doc}\n\n''')
         raise e
     except Exception as e:
-        print(f'An error occurred: {e}')
+        print(f'An error occurred: {e.reason}')
         raise e
     
 parser = argparse.ArgumentParser(
@@ -62,6 +81,7 @@ args = parser.parse_args()
 customMappingsData = readMappingFile(mappingFileName=args.customStandardFilename)
 
 conformityEndpoint = f"https://conformity.{args.region}.cloudone.trendmicro.com/api/compliance-standards/custom"
+
 response = callApi(apiUrl=conformityEndpoint, data=customMappingsData, apiKey=args.apiKey)
 
 print(json.dumps(obj=response, indent=4))
